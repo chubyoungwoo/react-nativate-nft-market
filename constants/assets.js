6f084dd216085df486551e6fc75382794629bae0
cbw <cbwstar@gmail.com>
2022-12-23 03:01:34 +0900

    최초커밋

---
+++ constants/assets.js
@@ -0,0 +1,45 @@
+import badge from "../assets/icons/badge.png";
+import eth from "../assets/icons/eth.png";
+import heart from "../assets/icons/heart.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+
+import logo from "../assets/images/logo3.png";
+import nft01 from "../assets/images/nft01.jpg";
+import nft02 from "../assets/images/nft02.jpg";
+import nft03 from "../assets/images/nft03.jpg";
+import nft04 from "../assets/images/nft04.jpg";
+import nft05 from "../assets/images/nft05.jpg";
+import nft06 from "../assets/images/nft06.png";
+import nft07 from "../assets/images/nft07.jpg";
+
+import person01 from "../assets/images/person001.png";
+import person02 from "../assets/images/person002.png";
+import person03 from "../assets/images/person003.png";
+import person04 from "../assets/images/person004.png";
+import person05 from "../assets/images/person005.png";
+import person06 from "../assets/images/person006.png";
+
+export default {
+  badge,
+  eth,
+  heart,
+  left,
+  search,
+
+  logo,
+  nft01,
+  nft02,
+  nft03,
+  nft04,
+  nft05,
+  nft06,
+  nft07,
+
+  person01,
+  person02,
+  person03,
+  person04,
+  person05,
+  person06,
+};
